8a64df9e4d2e4bfb0b02518c998b13806d1917c7
ferriyusra <ferisakuragi@gmail.com>
2026-02-11 15:15:26 +0700

    feat: setup entities and interfaces

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,76 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum Role {
+  ADMIN = 'admin',
+  BOARDER = 'boarder',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    length: 36,
+  })
+  uuid: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    length: 100,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    length: 15,
+  })
+  phone_number: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.BOARDER,
+  })
+  role: Role;
+
+  @Column()
+  photo: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  refresh_token: string;
+
+  @Column({
+    nullable: true,
+  })
+  refresh_token_expires_at: Date;
+
+  @CreateDateColumn({
+    name: 'created_at',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+  })
+  updated_at: Date;
+}
---
+++ src/interface/auth.interface.ts
@@ -0,0 +1,8 @@
+export interface IAuthUserPayload {
+  uuid: string;
+  name: string;
+  email: string;
+  role: string;
+  phone_number: string;
+  photo: string;
+}
