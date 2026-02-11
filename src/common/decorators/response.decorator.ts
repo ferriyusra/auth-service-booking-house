28a243fa9b7efd37094f46f871f0ac0d28ec0869
ferriyusra <ferisakuragi@gmail.com>
2026-02-11 10:13:18 +0700

    feat: auth serivice setup common decorator

---
+++ src/common/decorators/response.decorator.ts
@@ -0,0 +1,18 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const RESPONSE_MESSSAGE = 'response.message';
+export const RESPONSE_OPTS = 'response.opts';
+
+export type ResponseOpts = {
+  code?: number;
+  message?: string;
+  liftToken?: boolean;
+};
+
+export const ResponseMessage = (message: string) => {
+  SetMetadata(RESPONSE_MESSSAGE, message);
+};
+
+export const ResponseOption = (opts?: ResponseOpts) => {
+  SetMetadata(RESPONSE_OPTS, opts);
+};
